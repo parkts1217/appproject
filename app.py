b08889880b710f38c2f24e5759d7525cc6f74980
parkts1217 <parkts12174@gmail.com>
2026-06-16 16:41:52 +0900

    two

--- app.py
+++ app.py
@@ -1,19 +1,17 @@
 import streamlit as st
-import pandas as pd
-import numpy as np
 
-st.write("Streamlit supports a wide range of data visualizations, including [Plotly, Altair, and Bokeh charts](https://docs.streamlit.io/develop/api-reference/charts). 📊 And with over 20 input widgets, you can easily make your data interactive!")
+st.title("Hello TeaSoo 👋")
+st.markdown(
+    """ 
+    This is a playground for you to try Streamlit and have fun. 
 
-all_users = ["Alice", "Bob", "Charly"]
-with st.container(border=True):
-    users = st.multiselect("Users", all_users, default=all_users)
-    rolling_average = st.toggle("Rolling average")
+    **There's :rainbow[so much] you can build!**
+    
+    We prepared a few examples for you to get started. Just 
+    click on the buttons above and discover what you can do 
+    with Streamlit. 
+    """
+)
 
-np.random.seed(42)
-data = pd.DataFrame(np.random.randn(20, len(users)), columns=users)
-if rolling_average:
-    data = data.rolling(7).mean().dropna()
-
-tab1, tab2 = st.tabs(["Chart", "Dataframe"])
-tab1.line_chart(data, height=250)
-tab2.dataframe(data, height=250, use_container_width=True)
+if st.button("Send balloons!"):
+    st.balloons()
